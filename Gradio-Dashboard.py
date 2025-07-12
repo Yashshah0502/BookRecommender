809d337ae4ead614b129652a15155ae80c019ba3
Yashshah0502 <113469955+Yashshah0502@users.noreply.github.com>
2025-07-11 23:17:43 -0400

    Gradio Interface: Semantic Book Recommender

--- Gradio-Dashboard.py
+++ Gradio-Dashboard.py
@@ -77,3 +77,27 @@ def recommend_books(
         caption = f"{row['title']} by {authors_str}: {truncated_description}"
         results.append((row["large_thumbnail"], caption))
     return results
+
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
+        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
+
+    submit_button.click(fn = recommend_books,
+                        inputs = [user_query, category_dropdown, tone_dropdown],
+                        outputs = output)
+
+
+if __name__ == "__main__":
+    dashboard.launch()
